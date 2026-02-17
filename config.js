862b2707e4a0d2b2afb1a0ab87cba888614b9c21
BPGTerminal <bpgrandterminal@gmail.com>
2026-02-17 11:31:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,8 @@
 const CONFIG = {
     // Authentication API (handles login, registration, password reset)
     AUTH_API_URL: 'https://script.google.com/macros/s/AKfycbwYptsyGdy5gzfmCqFc6aJD4L3ajvzcseDImA8caeTFt02YwIdb-ox3FDPOP129MmWV/exec',
+
+    USERS_API_URL: 'https://script.google.com/macros/s/AKfycbwYptsyGdy5gzfmCqFc6aJD4L3ajvzcseDImA8caeTFt02YwIdb-ox3FDPOP129MmWV/exec',
     
     // Data API (handles passenger data submission)
     DATA_API_URL: 'https://script.google.com/macros/s/AKfycbyG73qv7vAoOANfZ2ceSUpzDMOAUZbjj3yB8reJMnxDJDka2uvcwszR9yw5rqHMGnRI/exec',
